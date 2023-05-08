9bdc2d6dafa68ae8eabd901f1ec3dee7eb8e4474
Jathoosh <73128006+Jathoosh@users.noreply.github.com>
2023-05-08 16:01:11 +0200

    Initialisation

---
+++ DataNavWeb/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const host = http://localhost
+const port = 3000
+
+// We'll use the public directory to serve the Vue App
+app.use(express.static('public'));
+
+app.listen(port, () => {
+ console.log(`App listening on ${host}:${port}`);
+});
