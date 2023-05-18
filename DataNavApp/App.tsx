90b04d53f8815c1cd96448a4e6f8b5ff6fadd7f0
NDU <61319301+Overshadow75@users.noreply.github.com>
2023-05-18 10:17:52 +0200

    Installation and first attempt of the accelerometer

--- DataNavApp/App.tsx
+++ DataNavApp/App.tsx
@@ -1,16 +1,43 @@
-import React from 'react';
-import {Text, StyleSheet} from 'react-native';
+import { useEffect, useState } from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+import { accelerometer } from 'react-native-sensors';
+import { Subscription } from 'rxjs';
+
 
 function App(): React.JSX.Element {
+  const [accelerometerData, setData] = useState({ x: 0, y: 0, z: 0 });
+
+  useEffect(() => {
+    let subscription: Subscription | null = null;
+    
+    if (accelerometer) {
+      subscription = accelerometer.subscribe(({ x, y, z }) => {
+        setData({ x, y, z });
+      });
+    }
+  
+    return () => {
+      if (subscription) {
+        subscription.unsubscribe();
+      }
+    };
+  }, [accelerometer]);
+  
+    
+
   return (
-    <Text style={styles.text}>Hello World !</Text>
+    <View>
+      <Text style={styles.text}>
+        Xo: {accelerometerData.x}, Y: {accelerometerData.y}, Z: {accelerometerData.z}
+      </Text>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   text: {
-    font : 'Arial',
     position: 'absolute',
+    fontSize: 24,
     top: 100,
     left: 100,
   },
