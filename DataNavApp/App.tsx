80fd4864fdda054eb8e1dcd6054a6afbb6d35332
NDU <61319301+Overshadow75@users.noreply.github.com>
2023-05-18 16:12:22 +0200

    Patch visibility data for android and ios (accelerometer)

--- DataNavApp/App.tsx
+++ DataNavApp/App.tsx
@@ -1,44 +1,72 @@
-import { useEffect, useState } from 'react';
-import { Text, StyleSheet, View } from 'react-native';
-import { accelerometer } from 'react-native-sensors';
-import { Subscription } from 'rxjs';
+import {useEffect, useState} from 'react';
+import {Text, StyleSheet, View, Platform} from 'react-native';
+import {
+  accelerometer,
+  setUpdateIntervalForType,
+  SensorTypes,
+} from 'react-native-sensors';
+import {Subscription} from 'rxjs';
 
+if (Platform.OS === 'android') {
+  setUpdateIntervalForType(SensorTypes.accelerometer, 100);
+}
 
 function App(): React.JSX.Element {
-  const [accelerometerData, setData] = useState({ x: 0, y: 0, z: 0 });
+  const [accelerometerData, setData] = useState({x: 0, y: 0, z: 0});
+  let error_message = 'Accelerometer data available';
 
-  useEffect(() => {
-    let subscription: Subscription | null = null;
-    
-    if (accelerometer) {
-      subscription = accelerometer.subscribe(({ x, y, z }) => {
-        setData({ x, y, z });
-      });
-    }
-  
-    return () => {
-      if (subscription) {
-        subscription.unsubscribe();
+  if (Platform.OS === 'android') {
+    useEffect(() => {
+      let subscription: Subscription | null = null;
+      if (accelerometer) {
+        subscription = accelerometer.subscribe(({x, y, z}) => {
+          setData({x, y, z});
+        });
       }
-    };
-  }, [accelerometer]);
-  
-    
+
+      return () => {
+        if (subscription) {
+          subscription.unsubscribe();
+        }
+      };
+    }, []);
+  } else {
+    error_message = 'accelerometer data not available';
+  }
 
   return (
     <View>
-      <Text style={styles.text}>
-        Xo: {accelerometerData.x}, Y: {accelerometerData.y}, Z: {accelerometerData.z}
-      </Text>
+      <Text style={styles.error}> {error_message}</Text>
+      <Text style={styles.textX}>X: {accelerometerData.x.toFixed(2)}</Text>
+      <Text style={styles.textY}>Y: {accelerometerData.y.toFixed(2)}</Text>
+      <Text style={styles.textZ}>Z: {accelerometerData.z.toFixed(2)}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  text: {
+  error: {
+    position: 'absolute',
+    fontSize: 15,
+    top: 20,
+    left: 20,
+  },
+  textX: {
     position: 'absolute',
     fontSize: 24,
     top: 100,
+    left: 100,
+  },
+  textY: {
+    position: 'absolute',
+    fontSize: 24,
+    top: 75,
+    left: 100,
+  },
+  textZ: {
+    position: 'absolute',
+    fontSize: 24,
+    top: 55,
     left: 100,
   },
 });
