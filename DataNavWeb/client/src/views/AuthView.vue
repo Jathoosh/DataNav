ac50e677ba0f5bf2850e8903dca0013463ea029b
Sylvain M <syl.migeon@gmail.com>
2023-05-21 13:18:47 +0200

    Update AuthView.vue

--- DataNavWeb/client/src/views/AuthView.vue
+++ DataNavWeb/client/src/views/AuthView.vue
@@ -1,28 +1,28 @@
 <template>
-  <div class="flex-vertical">
-    <div class="flex-horizontal">
+  <div class="flexVertical">
+    <div class="flexHorizontal">
 
       <br>
 
       <img src="../assets/logo.png" alt="logo" width="150" height="150">
 
       <form>
-        <div class="form-group">
+        <div class="formGroup">
           <label for="numServer">Numéro de la baie du serveur</label>
-          <input type="text" v-model="numServer" class="form-control" id="numServer" placeholder="Baie n°00139731...">
+          <input type="text" v-model="numServer" class="formControl" id="numServer" placeholder="Baie n°00139731...">
         </div>
 
-        <div class="form-group">
+        <div class="formGroup">
           <label for="serverRoad">Allée du serveur</label>
-          <input type="text" v-model="serverRoad" class="form-control" id="serverRoad" placeholder="Allée Gutenberg">
+          <input type="text" v-model="serverRoad" class="formControl" id="serverRoad" placeholder="Allée Gutenberg">
         </div>
 
         <button type="submit" class="btn btn-primary" v-on:click="codeGeneration">Générer un code</button>
       </form>
 
-      <div class="form-group">
+      <div class="formGroup">
           <label for="serverRoad">Code d'accès</label>
-          <input type="text" v-model="token" class="form-control" id="token" placeholder="Token">
+          <input type="text" v-model="token" class="formControl" id="token" placeholder="Token">
       </div>
 
       <br><br>
@@ -47,12 +47,12 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
-.flex-vertical{
+.flexVertical{
   display: flex;
   justify-content: center;
 }
 
-.flex-horizontal {
+.flexHorizontal {
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -62,25 +62,14 @@ export default {
     background-color: #F0F0F0;
 }
 
-.form-group {
+.formGroup {
     width: 400px;
     display: flex;
     flex-direction: column;
     justify-content: start;
     align-items: start;
 }
-.v-line {
-    width: 1px;
-    height: 100%;
-    background-color: #0390BF;
-}
-.h-line {
-    width: 70%;
-    height: 1px;
-    background-color: #000000;
-    margin-top: 20px;
-    margin-bottom: 10px;
-}
+
 button {
     padding: 10px 25px;
     font-size: 24px;
@@ -89,7 +78,7 @@ button {
     border: none;
     border-radius: 2px;
 }
-.form-group,button {
+.formGroup,button {
     margin-top: 30px;
 }
 label {
@@ -103,9 +92,10 @@ p {
     color : #0390BF;
 }
 
-.form-control{
+.formControl{
   border : solid 3px #D9D9D9;
   height : 40px;
+  border-radius: 2px;
 }
 
 </style>
