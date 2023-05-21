3c8e78ec1175c75e5dc7c1849ab54caf72a773db
Sylvain M <syl.migeon@gmail.com>
2023-05-21 13:03:56 +0200

    Update AuthView.vue

--- DataNavWeb/client/src/views/AuthView.vue
+++ DataNavWeb/client/src/views/AuthView.vue
@@ -1,30 +1,33 @@
 <template>
-  <div class="flex-container">
+  <div class="flex-vertical">
+    <div class="flex-horizontal">
 
-    <br>
+      <br>
 
-    <img src="../assets/logo.png" alt="logo" width="150" height="150">
+      <img src="../assets/logo.png" alt="logo" width="150" height="150">
 
-    <form>
+      <form>
+        <div class="form-group">
+          <label for="numServer">Numéro de la baie du serveur</label>
+          <input type="text" v-model="numServer" class="form-control" id="numServer" placeholder="Baie n°00139731...">
+        </div>
+
+        <div class="form-group">
+          <label for="serverRoad">Allée du serveur</label>
+          <input type="text" v-model="serverRoad" class="form-control" id="serverRoad" placeholder="Allée Gutenberg">
+        </div>
+
+        <button type="submit" class="btn btn-primary" v-on:click="codeGeneration">Générer un code</button>
+      </form>
+
       <div class="form-group">
-        <label for="numServer">Numéro de la baie du serveur</label>
-        <input type="text" v-model="numServer" class="form-control" id="numServer" placeholder="Baie n°00139731...">
+          <label for="serverRoad">Code d'accès</label>
+          <input type="text" v-model="token" class="form-control" id="token" placeholder="Token">
       </div>
 
-      <div class="form-group">
-        <label for="serverRoad">Allée du serveur</label>
-        <input type="text" v-model="serverRoad" class="form-control" id="serverRoad" placeholder="Allée Gutenberg">
-      </div>
+      <br><br>
 
-      <button type="submit" class="btn btn-primary" v-on:click="codeGeneration">Générer un code</button>
-    </form>
-
-    <div class="form-group">
-        <label for="serverRoad">Code d'accès</label>
-        <input type="text" v-model="token" class="form-control" id="token" placeholder="Token">
     </div>
-
-    <br><br>
 
   </div>
 </template>
@@ -43,18 +46,24 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.flex-container {
+
+.flex-vertical{
+  display: flex;
+  justify-content: center;
+}
+
+.flex-horizontal {
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    width: 100%;
+    width: 95%;
     height: 100%;
     background-color: #F0F0F0;
 }
 
 .form-group {
-    width: 300px;
+    width: 400px;
     display: flex;
     flex-direction: column;
     justify-content: start;
@@ -93,4 +102,10 @@ input {
 p {
     color : #0390BF;
 }
+
+.form-control{
+  border : solid 3px #D9D9D9;
+  height : 40px;
+}
+
 </style>
