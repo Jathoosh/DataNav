1a15321380498ab7eb15e147927435d0cbfc3cc7
Jathoosh <73128006+Jathoosh@users.noreply.github.com>
2023-05-25 19:49:01 +0200

    ça marche et c'est divisé

--- DataNavApp/.eslintrc.js
+++ DataNavApp/.eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-uses-react": "off",
+    "react-native/no-inline-styles": "off",
+  }
 };
---
+++ DataNavApp/views/MapsInfo.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import {View, Text} from 'react-native';
+
+function MapsInfo() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Text>Maps Info</Text>
+    </View>
+  );
+}
+
+export default MapsInfo;
