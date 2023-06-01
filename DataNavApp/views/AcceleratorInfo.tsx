9c2dfb35bad2f9abd7fdd5c01a8805d1962e99cb
NDU <61319301+Overshadow75@users.noreply.github.com>
2023-06-01 22:55:12 +0200

    comment if statement (delete at the end of sprint 3)

--- DataNavApp/views/AcceleratorInfo.tsx
+++ DataNavApp/views/AcceleratorInfo.tsx
@@ -5,25 +5,25 @@ import {accelerometer} from 'react-native-sensors';
 
 function AcceleratorInfos({navigation}) {
   const [accelerometerData, setData] = React.useState({x: 0, y: 0, z: 0});
-  let error_message = 'Accelerometer data available';
-  if (Platform.OS === 'android') {
-    React.useEffect(() => {
-      let subscription: Subscription | null = null;
-      if (accelerometer) {
-        subscription = accelerometer.subscribe(({x, y, z}) => {
-          setData({x, y, z});
-        });
-      }
+  // TODO : remove if statement at the end of SPRINT 3
+  //if (Platform.OS === 'android') {
+  React.useEffect(() => {
+    let subscription: Subscription | null = null;
+    if (accelerometer) {
+      subscription = accelerometer.subscribe(({x, y, z}) => {
+        setData({x, y, z});
+      });
+    }
 
-      return () => {
-        if (subscription) {
-          subscription.unsubscribe();
-        }
-      };
-    }, []);
-  } else {
+    return () => {
+      if (subscription) {
+        subscription.unsubscribe();
+      }
+    };
+  }, []);
+  /*} else {
     error_message = 'accelerometer data not available';
-  }
+  }*/
 
   return (
     <View
@@ -33,13 +33,15 @@ function AcceleratorInfos({navigation}) {
         justifyContent: 'center',
       }}>
       <Text>Accelerator Infos</Text>
-      <Text style={styles.error}> {error_message}</Text>
       <Text style={styles.textX}>X: {accelerometerData.x.toFixed(2)}</Text>
       <Text style={styles.textY}>Y: {accelerometerData.y.toFixed(2)}</Text>
       <Text style={styles.textZ}>Z: {accelerometerData.z.toFixed(2)}</Text>
       <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
       <Button title="Go to Maps" onPress={() => navigation.navigate('Maps')} />
-      <Button title="Go to Login" onPress={() => navigation.navigate('Login')} />
+      <Button
+        title="Go to Login"
+        onPress={() => navigation.navigate('Login')}
+      />
     </View>
   );
 }
