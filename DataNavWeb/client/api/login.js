2925cb78af6c0e7623e6a778a723ec15856c9040
Sylvain M <syl.migeon@gmail.com>
2023-06-04 19:52:41 +0200

    Login process
    Not tested yet

--- DataNavWeb/client/api/login.js
+++ DataNavWeb/client/api/login.js
@@ -1,8 +1,9 @@
 export default async function loginFunc(data) {
-    // const username = data.username;
+    const username = data.username;
+    console.log("From Login.js: " + data.username);
     console.log("From Login.js: " + data.password);
-    //return await axios.post('/api/login', {username: username, password: password}); TODO: uncomment this line when backend is ready
-    return await new Promise(r => setTimeout(r, 3000)).then(() => {
-        return {status: 200, data: {company: "Equinix"}};
-    });
+    return await axios.post('/api/login', {username: username, password: password}); //TODO: uncomment this line when backend is ready
+    // return await new Promise(r => setTimeout(r, 3000)).then(() => {
+    //     return {status: 200, data: {company: "Equinix"}};
+    // });
 }
