005aa996e233c6cebabb0a4fac1a42efd8d48640
Sylvain M <syl.migeon@gmail.com>
2023-06-04 19:52:41 +0200

    Login process
    Not tested yet

--- DataNavWeb/index.js
+++ DataNavWeb/index.js
@@ -33,6 +33,40 @@ app.get('/', (req, res) => {
     res.render('index.html');
 });
 
+// Login Process
+app.post('/api/login', (req, res) => {
+    const { username, password } = req.body;
+  
+    sequelize
+    .query('SELECT * FROM users WHERE username = :username', {
+        replacements: { username: username },
+        type: QueryTypes.SELECT,
+    })
+    .then((results) => {
+        if (results.length === 0) {
+        res.status(401).send({ error: 'Nom d\'utilisateur incorrect' });
+        } else {
+        const user = results[0];
+        const hashedPassword = user.password;
+
+        bcrypt.compare(password, hashedPassword, (err, result) => {
+            if (err) {
+            console.error('Erreur lors de la comparaison des mots de passe', err);
+            res.status(500).send({ error: 'Erreur de serveur' });
+            } else if (result) {
+            res.status(200).send({ message: 'Authentification réussie' });
+            } else {
+            res.status(401).send({ error: 'Mot de passe incorrect' });
+            }
+        });
+    }})
+    .catch((err) => {
+        console.error('Erreur lors de la recherche de l\'utilisateur', err);
+        res.status(500).send({ error: 'Erreur de serveur' });
+    });
+});
+
+// Code generation
 app.post('/api/codeGenerator', (req, res) => {
     const random = require('random-string-generator');
     const numServer = req.body.numServer;
