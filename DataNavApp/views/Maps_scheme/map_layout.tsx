7995eb1980d8571253c15685a66e86f2409c79a5
NDU <61319301+Overshadow75@users.noreply.github.com>
2023-06-29 17:58:11 +0200

    generate map

--- DataNavApp/views/Maps_scheme/map_layout.tsx
+++ DataNavApp/views/Maps_scheme/map_layout.tsx
@@ -3,44 +3,51 @@ import {Svg, Path} from 'react-native-svg';
 import jsonData from './data2.json';
 
 type Road = {
-  id: Int16Array;
+  id: Number;
   direction: String;
-  start_node: Int16Array;
-  end_node: Int16Array;
-  length: Int16Array;
-}
+  start_node: Number;
+  end_node: Number;
+  length: Number;
+};
 
 type Intersection = {
-  "id": Int16Array;
-  "x": Int16Array;
-  "y": Int16Array;
-  "roads": Int16Array[];
-}
+  id: Number;
+  x: Number;
+  y: Number;
+  roads: Number[];
+};
 
 type JSONMap = {
   roads: Road[];
   intersections: Intersection[];
-}
+};
 
 function SvgComponent() {
+  const jsonMap: JSONMap = jsonData as unknown as JSONMap;
 
-  const jsonMap:JSONMap = jsonData as unknown as JSONMap;
-
-  const handleNavigateToMaps = (jsonData : JSONMap) => {
-    for (let index = 0; index < jsonData.roads.length; index++) {
-      console.log("HUHO");
-      
-    }
+  const getX = (id: Number, json_inter: JSONMap) => {
+    const interX = json_inter.intersections.find((item: any) => item.id === id);
+    return interX === undefined ? 0 : interX.x;
   };
-  console.log(jsonMap);
-  handleNavigateToMaps(jsonMap);
+
+  const getY = (id: Number, json_inter: JSONMap) => {
+    const interY = json_inter.intersections.find((item: any) => item.id === id);
+    return interY === undefined ? 0 : interY.y;
+  };
 
   return (
     <Svg width="100%" height="100%">
       {jsonData.roads.map(item => (
         <Path
           key={item.id}
-          d={    "M"+ getX(item.start_node)+getY(item.start_node)+item.direction+ item.length}
+          d={
+            'M' +
+            getX(item.start_node, jsonMap) +
+            ' ' +
+            getY(item.start_node, jsonMap) +
+            item.direction +
+            item.length
+          }
           fill="none"
           stroke="#000"
           strokeWidth="5.00"
